f3b0b381cde654737e87e486dcaad71b299f6bc7
Adam <Adam>
2023-05-09 16:26:06 +0200

    ex07

---
+++ module00/ex07/filterwords.py
@@ -0,0 +1,22 @@
+import sys
+import string
+
+def filterword(s : str, number : int) :
+    s = [i for i in s if i not in string.punctuation]
+    s = ''.join(s).split()
+    print([i for i in s if len(i) > number])
+    # x = string.punctuation
+    # L = s.split("or")
+    # print(x, L)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3 :
+        print("ERROR")
+        exit(1)
+    try : 
+        number = int(sys.argv[2])
+    except ValueError :
+        print("ERROR")
+        exit(1)    
+    filterword(sys.argv[1], number)
+    
